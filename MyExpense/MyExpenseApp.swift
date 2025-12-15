0f65d0514327898bdb126102fc1f8b3e51d87dec
berkahhariini999-rgb <berkahhariini999@gmail.com>
2025-12-15 14:14:37 +0700

    Initial Commit

---
+++ MyExpense/MyExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyExpenseApp.swift
+//  MyExpense
+//
+//  Created by Iqbal Alhadad on 15/12/25.
+//
+
+import SwiftUI
+
+@main
+struct MyExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
